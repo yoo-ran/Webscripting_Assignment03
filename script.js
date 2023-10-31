f9c3b0647fe57c97c65c6f1677d477a7c9a7f463
YooranKim <yuranm@naver.com>
2023-10-31 16:05:14 -0700

    start

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 const estimateBtn = document.querySelector("#estimate");
-let shipping =document.querySelector("#shipping").value;
-let certificate = document.querySelector("#certificate").checked;
-let originalPackage = document.querySelector("#original-package").checked;
-let tax = document.querySelector("#taxable").checked;
-
-let priceArr = []
-for (let i=1; i < 6; i++) {
-    priceArr.push(`item-${i}-input`);
-}
-
-const calcurateChecked = (tax,num) =>{
-    if(tax!="tax"){
-        console.log(tax);
-        let itemArr = document.querySelectorAll(".itemPrice");
-        itemArr.forEach(item => {
-            let price = Number(item.value)+Number(num)
-        })
-    }else {
-        let itemArr = document.querySelectorAll(".itemPrice")
-        itemArr.forEach(item => {
-            let price = Math.floor(item.value*1.12)
-        });
-    }
-}
 
 
-
-const test = () => {
-    if (originalPackage) {
-        calcurateChecked("package", 9.95)
+const estimate = () => {
+    let priceArr = document.querySelectorAll(".itemPrice")
+    let resultArr = document.querySelectorAll(".result")
+    for (let i = 0; i < priceArr.length; i++) {
+        calculate(Number(priceArr[i].value),resultArr[i])
     }
     
-    if (certificate) {
-        calcurateChecked("certificate", 2.75)
-    }
 }
 
-estimateBtn.addEventListener("click",test);
+const calculate = (price,result)=> {
+    let shipping = document.querySelector("#shipping").value;
+    let outsideCost = Number(document.querySelector("#outside-costs").value)
+    let certificate = document.querySelector("#certificate").checked;
+    let originalPackage = document.querySelector("#original-package").checked;
+    let tax = document.querySelector("#taxable").checked;
+
+    if(outsideCost>=0 && outsideCost<=50 && shipping){
+        switch (shipping) {
+            
+            case "REG":
+                price += 5.50;
+                break;
+            
+            case "EXP":
+                price += 15.75;
+                break;
+            
+            case "OVER":
+                price += 29.95;
+                break;
+        }
+        price += outsideCost;
+
+        if(certificate){
+            price += 2.75;
+        }
+    
+        if(originalPackage){
+            price+=9.95;
+        }
+
+        if(tax){
+            price *=1.12
+        }
+        result.innerHTML = "$" + Math.round(price);
+
+    }else {
+        document.querySelector("#shipping-warning").innerHTML = "Outside Costs is invalidate"
+        document.querySelector("#outside-warning").innerHTML = "Outside Costs is invalidate"
+    }
+
+
+}
+
+
+estimateBtn.addEventListener("click",estimate);
